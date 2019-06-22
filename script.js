c058ef1e9ce412d14190b54812e68fede877dafe
sehrishbashir <49878787+sehrishbashir@users.noreply.github.com>
2019-06-22 12:15:57 +0500

    add js problems

---
+++ script.js
@@ -0,0 +1,183 @@
+/*First Problem
+
+pseudocode
+Agr gaari ki speed 60 se zyada hai,
+toh matlab overspeeding kr rahi hai,
+warna overspeeding nahi kar rhi
+
+const speedLimit = 60;
+let carSpeed = 50;
+if (carSpeed > speedLimit){
+    console.log("OVERSPEEDING");
+} else{
+    console.log("NOT");
+}*/
+
+
+
+/*Second Problem
+
+pseudocode
+Agr logged in hai,
+toh kaho "Welcome, you!",
+warna kaho "Please log in to continue"
+
+if(isLoggesIn = true){
+    alert("Welcome");
+} else{
+    alert("please logged in to continue");
+}*/
+
+
+
+/*Third Problem
+
+pseudocode
+Agr temperature 40 se zyada hai,
+toh kaho "IMPORTANT ANNOUNCEMENT!!!
+UIT is closed tomorrow due to heat wave",
+
+const temperature = 40;
+let todayTemperature = 50;
+if (todayTemperature > temperature) {
+    alert("IMPORTANT ANNOUNCEMENT!!! UIT is closed tomorrow due to heat wave");
+} else{
+    alert("it should do nothing");
+}*/
+
+
+
+/*Forth Problem
+
+pseudocode
+Agr bank account m kamas kam Rs. 1000000 hain,
+toh kaho "Congratulations! You are a millionaire"
+warna kaho "I'm sorry, you are not a millionaire"
+
+const amount = 3000;
+let bankAmount =  1000000;
+if (amount > bankAmount){
+    alert("Congratulations! You are a millionaire");
+} else{
+    alert("I'm sorry, you are not a millionaire");
+}*/
+
+
+
+/*Fifth Problem
+
+pseudocode
+Agr car electric hai,
+toh matlab Tesla company ne banai hai
+warna Tesla ne nahi banai
+
+if (isCarElectric = true){
+    alert("made by Tesla Company");
+} else{
+    alert("doesn't made by Tesla Company");
+}*/
+
+
+
+/*Sixth Problem
+
+pseudocode
+Agr abhi wala score puranay high score se zyada hai,
+toh kaho "Congratulations!! Your new high score is <user's high score>"
+warna kaho "Try Again. You just need <the number of more points needed to beat the high score> to win!"
+
+var nowScore = 90;
+let previousScore = 100;
+if (nowScore > previousScore){
+    alert("Congratulations!! Your new high score is <user's high score>");
+} else{
+    alert("Try Again. You just need <the number of more points needed to beat the high score> to win!");
+}*/
+
+
+
+/*Seventh Problem
+
+pseudocode
+Agr number even hai (matlab 2 se divide karne p iss ka remainder zero hai),
+toh kaho "This is EVEN"
+warna kaho "This is ODD"
+
+var number = 6;
+let even = number % 2;
+if (even = 0) {
+    alert("This is even");
+} else {
+alert("This is odd");
+}                   not solve*/
+
+
+/*Eighth Problem
+
+pseudocode
+If the user has less than Rs. 1 balance,
+the program prints Moazziz Sarif, aap ka mojooda 
+balance iss call k lye naa kaafi hai. 
+Please re-charge karain If the user has Rs. 1 to 10, 
+it prints Moazziz sarif, aap ka balance khatam honay 
+wala hai , and then prints Ring Ring!!. 
+If the user has greater than Rs. 10 balance, 
+the program prints Ring Ring
+
+let balance = 100;
+if (balance < 1){
+    alert("Moazziz Sarif, aap ka mojooda balance iss call k lye naa kaafi hai.");
+} 
+if (balnace >= 1 && balance <= 10){
+    alert("Moazziz sarif, aap ka balance khatam honay wala hai");
+}
+if (balance > 10){
+    alert("Ring Ring!!.");
+}                          not solve*/
+
+
+/*Ninth Problem
+
+pseudocode
+TechKaro admission test is designed to admit all 
+students who meet at least one of the following criteria:
+
+Has scored more than 10 points in Math test
+Has scored more than 20 points in Computer test
+Has scored more than 8 points in English test
+Write a program that checks a student's marks 
+and tells if he has passed the admission test or not.
+
+var mathScored = 9;
+var computerScored = 9;
+var englishScore = 7;
+
+if (mathScored > 10 || computerScored > 20 || englishScored > 8) {
+    alert("Congratulation! you are passed the admission test");
+} else {
+    alert("you are not passed the admission test");
+}          not solve*/
+
+
+/*Tenth Problem
+
+pseudocode
+TechKaro admission test is designed to admit all students 
+who meet all of the following criteria:
+
+Has scored more than 10 points in Math test
+Has scored more than 20 points in Computer test
+Has scored more than 8 points in English test 
+Write a program that checks a student's marks and 
+declares if he has passed the admission test or not.*/
+
+
+
+
+
+
+
+
+
+
+
